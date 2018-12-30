467e3a9e0b19ef3190552602530af6af885b9bb8
AmauryMBA <amaury.tobiasqr@gmail.com>
2018-12-30 14:32:19 -0600

    First Commit

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: "MainPage",
+    script: "./bin/www"
+  }],
+  "deploy": {
+    "production": {
+      //"key"  : "/path/to/key.pem",
+      "user": "amaurytobias",
+      "host": "amaurytq.me",
+      "ref": "origin/master",
+      "repo": "https://github.com/amaury-tobias/testpm2.git",
+      "path": "/home/amaurytobias/production/MainPage",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+    },
+  }
+}
